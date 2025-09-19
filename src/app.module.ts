67005b9b61c5e3ea41c334f14f9c6aa5ce481256
Матвей <matvey-kvasov@mail.ru>
2025-09-19 17:35:53 +0400

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomModule } from './room/room.module';
 import { ScheduleModule } from './schedule/schedule.module';
-import { SheduleController } from './shedule/shedule.controller';
 
 @Module({
 	imports: [RoomModule, ScheduleModule],
-	controllers: [AppController, SheduleController],
+	controllers: [AppController],
 	providers: [AppService],
 })
 export class AppModule {}
